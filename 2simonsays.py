f795571c94090a744d24e188ecc4ba7c8dfd298b
YRANBA <jjjw225@outlook.com>
2018-07-24 15:39:34 -0500

    changes

--- 2simonsays.py
+++ 2simonsays.py
@@ -3,16 +3,34 @@ import LEDRGB as LED
 import time
 import random
 
-colors = [ 'R' ,  'G',  'B' ,  'Y']
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+buzz_pin = 32
+
+GPIO.setup(buzz_pin,GPIO.OUT)
+Buzz = GPIO.PWM(buzz_pin, 1000)
+
+
+colors = [ 'R' ,  'G',  'B' ,  'Y'] 
 R_pin = 11
 G_pin = 12
-B_pin  = 13
+B_pin  = 13 
+frequencies = [220, 440, 880, 1760]
+previous = []
 LED.setup(R_pin, G_pin, B_pin)
 def append_list ( ):
 		while True:
-				n = random.randint (0,3)
-				LED.setColor (colors[n])
-				time.sleep(1)
+				n =random.randint (0,3)
+				previous.append(n)
+				for i in previous:
+					LED.setColor (colors[i])
+					Buzz.ChangeFrequency(frequencies[i])
+					Buzz.start(25)
+					time.sleep(1)
+					Buzz.stop( )
+					LED.noColor( )
+					time.sleep(1)
 
 if __name__ == '__main__' :
 		try:
@@ -20,4 +38,5 @@ if __name__ == '__main__' :
 		except KeyboardInterrupt:
 				print "goodbye"
 				LED.destroy( )
+				Buzz.stop( )
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # simon_says
 Colour sequence game for raspberry pi!
+v0.1.0
